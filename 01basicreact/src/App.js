864f17bda37f48b359274547be3faccf5274ee4f
David Vasconcelos <dsv10@hotmail.com>
2024-01-04 00:40:07 -0300

    react fundation

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    // <> </> You must return only one tag, this trick is to prevent any hmtl behavior
+    <>
+      <h2>classic react app</h2>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./youtube"
+
+function App() {
+  return (
+    // <> </> You must return only one tag, this trick is to prevent any hmtl behavior
+    <> 
+      <h1>Vite react app</h1>
+      <Youtube />
+    </>
+  )
+}
+
+export default App
