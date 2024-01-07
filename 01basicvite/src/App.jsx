fe20b7d3c452721df5577cf07da8b418c9cdee62
David Vasconcelos <dsv10@hotmail.com>
2024-01-06 21:02:10 -0300

    custom react under the hood

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,13 +1,16 @@
-import Youtube from "./youtube"
+import Youtube from "./youtube";
 
 function App() {
+  const username = "david";
+  const logic = username === "david" ? "yes" : "no";
   return (
     // <> </> You must return only one tag, this trick is to prevent any hmtl behavior
-    <> 
-      <h1>Vite react app</h1>
+    <>
+      <h1>Vite react app {2 + 2}</h1>
+      <h1>Vite react app {logic}</h1>
       <Youtube />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,7 +1,42 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
 
+//  See CustomReact
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "http://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />  
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    Click me to visit Google
+  </a>
+);
+
+//ReactDOM.createRoot(document.getElementById("root")).render(AnotherElement);
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "http://google.com",
+    target: "_blank",
+  },
+  "Click me to visit Google"
+);
+
+//ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
