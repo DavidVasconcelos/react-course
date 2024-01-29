bc6856b5a56a7c86b68294301741b6120ef21425
David Vasconcelos <dsv10@hotmail.com>
2024-01-29 19:59:42 -0300

    Use storage
    Context and functions

---
+++ 10todoContextLocal/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+import { TodoProvider } from "./contexts";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <h1 className="text-3xl font-bold unserline">Hello World!</h1>
+    </TodoProvider>
+  );
+}
+
+export default App;
